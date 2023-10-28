4c7d57db2cf2899c6ed9afc2840ab032dce55794
elmoconh <helmer.morales1@gmail.com>
2023-10-28 13:45:52 -0300

    add values for arrow function

--- 03-ArrowFunctions.js
+++ 03-ArrowFunctions.js
@@ -8,4 +8,16 @@ Fn.prototype.metodo = function Hola(){
 }
 
 const r = new Fn();
-console.log(r.Hola);
+console.log(r);
+
+const ArrowFn = () => {
+    this.prop = "funcion flecha"
+    console.log("Hola desde una" + this.prop);
+    return "Hola";
+}
+
+//nota no se puede usar la palabra resevada new con las funciones de flecha porque no tienen un this propio y no se puede usar el operador new
+//const r1 =new ArrowFn();
+
+const r1 = ArrowFn();
+console.log(r1);
